434efe9a16faa2542ffd5d15664c7d3019604edc
Krzysztof Dąbrowski <k.fajny@gmail.com>
2018-05-07 22:56:35 +0200

    Poprawiony interfejs

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -37,85 +37,106 @@
             this.userInputTexBox = new System.Windows.Forms.TextBox();
             this.sprawdzenieButton = new System.Windows.Forms.Button();
             this.poprawnoscLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // losujDzialanieButton
             // 
-            this.losujDzialanieButton.Location = new System.Drawing.Point(183, 217);
+            this.losujDzialanieButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.losujDzialanieButton.Location = new System.Drawing.Point(27, 161);
             this.losujDzialanieButton.Name = "losujDzialanieButton";
-            this.losujDzialanieButton.Size = new System.Drawing.Size(75, 94);
+            this.losujDzialanieButton.Size = new System.Drawing.Size(95, 94);
             this.losujDzialanieButton.TabIndex = 0;
-            this.losujDzialanieButton.Text = "losuj nastepne dzialanie";
+            this.losujDzialanieButton.Text = "Następne działanie";
             this.losujDzialanieButton.UseVisualStyleBackColor = true;
             this.losujDzialanieButton.Click += new System.EventHandler(this.button1_Click);
             // 
             // liczba1Label
             // 
             this.liczba1Label.AutoSize = true;
-            this.liczba1Label.Location = new System.Drawing.Point(163, 137);
+            this.liczba1Label.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.liczba1Label.Location = new System.Drawing.Point(20, 74);
             this.liczba1Label.Name = "liczba1Label";
-            this.liczba1Label.Size = new System.Drawing.Size(40, 13);
+            this.liczba1Label.Size = new System.Drawing.Size(36, 37);
             this.liczba1Label.TabIndex = 1;
-            this.liczba1Label.Text = "liczba1";
+            this.liczba1Label.Text = "5";
             // 
             // liczba2Label
             // 
             this.liczba2Label.AutoSize = true;
-            this.liczba2Label.Location = new System.Drawing.Point(283, 137);
+            this.liczba2Label.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.liczba2Label.Location = new System.Drawing.Point(160, 74);
             this.liczba2Label.Name = "liczba2Label";
-            this.liczba2Label.Size = new System.Drawing.Size(40, 13);
+            this.liczba2Label.Size = new System.Drawing.Size(36, 37);
             this.liczba2Label.TabIndex = 2;
-            this.liczba2Label.Text = "liczba2";
+            this.liczba2Label.Text = "7";
             // 
             // znakLabel
             // 
             this.znakLabel.AutoSize = true;
-            this.znakLabel.Location = new System.Drawing.Point(223, 137);
+            this.znakLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.znakLabel.Location = new System.Drawing.Point(87, 74);
             this.znakLabel.Name = "znakLabel";
-            this.znakLabel.Size = new System.Drawing.Size(30, 13);
+            this.znakLabel.Size = new System.Drawing.Size(37, 37);
             this.znakLabel.TabIndex = 3;
-            this.znakLabel.Text = "znak";
+            this.znakLabel.Text = "+";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(357, 136);
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label4.Location = new System.Drawing.Point(220, 74);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(13, 13);
+            this.label4.Size = new System.Drawing.Size(37, 37);
             this.label4.TabIndex = 4;
             this.label4.Text = "=";
             // 
             // userInputTexBox
             // 
-            this.userInputTexBox.Location = new System.Drawing.Point(403, 134);
+            this.userInputTexBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.userInputTexBox.Location = new System.Drawing.Point(275, 71);
             this.userInputTexBox.Name = "userInputTexBox";
-            this.userInputTexBox.Size = new System.Drawing.Size(100, 20);
+            this.userInputTexBox.Size = new System.Drawing.Size(100, 44);
             this.userInputTexBox.TabIndex = 5;
             this.userInputTexBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.userInputTexBox_KeyDown);
             // 
             // sprawdzenieButton
             // 
-            this.sprawdzenieButton.Location = new System.Drawing.Point(403, 200);
+            this.sprawdzenieButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.sprawdzenieButton.Location = new System.Drawing.Point(275, 161);
             this.sprawdzenieButton.Name = "sprawdzenieButton";
-            this.sprawdzenieButton.Size = new System.Drawing.Size(75, 110);
+            this.sprawdzenieButton.Size = new System.Drawing.Size(95, 94);
             this.sprawdzenieButton.TabIndex = 6;
-            this.sprawdzenieButton.Text = "sprawdzenie";
+            this.sprawdzenieButton.Text = "Sprawdź";
             this.sprawdzenieButton.UseVisualStyleBackColor = true;
             this.sprawdzenieButton.Click += new System.EventHandler(this.button2_Click);
             // 
             // poprawnoscLabel
             // 
             this.poprawnoscLabel.AutoSize = true;
-            this.poprawnoscLabel.Location = new System.Drawing.Point(311, 249);
+            this.poprawnoscLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.poprawnoscLabel.Location = new System.Drawing.Point(163, 229);
             this.poprawnoscLabel.Name = "poprawnoscLabel";
-            this.poprawnoscLabel.Size = new System.Drawing.Size(0, 13);
+            this.poprawnoscLabel.Size = new System.Drawing.Size(61, 20);
             this.poprawnoscLabel.TabIndex = 7;
+            this.poprawnoscLabel.Text = "Dobrze";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(21, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(173, 31);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Policz ile to:";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.ClientSize = new System.Drawing.Size(409, 286);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.poprawnoscLabel);
             this.Controls.Add(this.sprawdzenieButton);
             this.Controls.Add(this.userInputTexBox);
@@ -144,6 +165,7 @@
         private System.Windows.Forms.TextBox userInputTexBox;
         private System.Windows.Forms.Button sprawdzenieButton;
         private System.Windows.Forms.Label poprawnoscLabel;
+        private System.Windows.Forms.Label label1;
     }
 }
 
