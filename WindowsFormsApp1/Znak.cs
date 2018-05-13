3b832117db0c7674f2aadb1638c7aaafedad6f2f
Krzysztof Dąbrowski <k.fajny@gmail.com>
2018-05-13 10:35:28 +0200

    Znak ma teraz własną klasę

---
+++ WindowsFormsApp1/Znak.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    class Znak
+    {
+        private enum Znaki
+        {
+            Plus,
+            Minus,
+            Razy,
+            Podzielic
+        };
+        private Znaki znak;
+
+        private Znak(Znaki znak)
+        {
+            this.znak = znak;
+        }
+
+        public Znak(string znak)
+        {
+            switch (znak)
+            {
+                case "+":
+                    this.znak = Znaki.Plus;
+                    break;
+                case "-":
+                    this.znak = Znaki.Minus;
+                    break;
+                case "*":
+                    this.znak = Znaki.Razy;
+                    break;
+                case "/":
+                    this.znak = Znaki.Podzielic;
+                    break;
+
+                default:
+                    throw new Exception("Zamiana z niewlasciwego znaku");
+            }
+        }
+
+        public override string ToString()
+        {
+            switch (znak)
+            {
+                case Znaki.Plus:
+                    return "+";
+                case Znaki.Minus:
+                    return "-";
+                case Znaki.Razy:
+                    return "*";
+                case Znaki.Podzielic:
+                    return "/";
+
+                default:
+                    return "";
+            }
+        }
+
+        public static Znak LosowyZnak()
+        {
+            Random random = new Random();
+
+            return new Znak((Znaki)random.Next(0, 4));
+        }
+
+        public Znak Plus()
+        {
+            return new Znak(Znaki.Plus);
+        }
+
+        public Znak Minus()
+        {
+            return new Znak(Znaki.Minus);
+        }
+
+        public Znak Podzielic()
+        {
+            return new Znak(Znaki.Podzielic);
+        }
+
+        public Znak Razy()
+        {
+            return new Znak(Znaki.Razy);
+        }
+    }
+}
