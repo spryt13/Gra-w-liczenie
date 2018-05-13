66fd9d9e47ae8c0e5c7dab3c7b64384810529e2a
Krzysztof Dąbrowski <k.fajny@gmail.com>
2018-05-13 11:19:07 +0200

    Klasa Rownanie

---
+++ WindowsFormsApp1/Rownanie.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    class Rownanie
+    {
+        private int a;
+        private int b;
+        private Znak znak;
+
+        public Rownanie(int a, Znak znak, int b)
+        {
+            this.a = a;
+            this.znak = znak;
+            this.b = b;
+        }
+
+        public int A => a;
+        public int B => B;
+        public Znak ZnakRownania => znak;
+
+        public int Oblicz()
+        {
+            if (znak == Znak.Plus())
+                return a + b;
+            if (znak == Znak.Minus())
+                return a - b;
+            if (znak == Znak.Podzielic())
+                return a / b;
+            if (znak == Znak.Razy())
+                return a * b;
+
+            return 0;
+        }
+
+        public static Rownanie LosoweRownanie(int maksymalnaWielkoscLiczb = 10)
+        {
+            if (maksymalnaWielkoscLiczb < 1)
+                throw new Exception("maksymalnaWielkoscLiczb musi byc >= 1");
+            Random random = new Random();
+
+            int a = random.Next(1, maksymalnaWielkoscLiczb);
+            int b = random.Next(1, maksymalnaWielkoscLiczb);
+
+            return new Rownanie(a, Znak.LosowyZnak(), b);
+        }
+    }
+}
